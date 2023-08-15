a3106f49f96e0dd979a7bc099903720a068632d3
Lirik-Keltran <keltran.987@gmail.com>
2023-08-16 06:33:42 +1100

    фикс

--- bin/main.ml
+++ bin/main.ml
@@ -1,8 +1,12 @@
-open Ast
-open Ast.Eval
+(* open Ast *)
+(* open Ast.Eval *)
+open Vm
 
-let _ =
+(* let _ =
   let text = Core.In_channel.read_all "./script.crs" in
   let lexbuf = Lexing.from_string text in
   let ast = Parser.prog Lexer.read lexbuf in
-  eval ast Std.std
+  eval ast Std.std *)
+
+let _ = let _ = virt |> run_vm in
+  print_vm_heap virt
